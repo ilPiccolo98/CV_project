fdca053f08439f09c3f2d52c73337645e9897714
Giuseppe Piccolo <giuseppe.piccolo@protonmail.com>
2024-01-16 12:15:08 +0100

    cleaned the code

--- main.py
+++ main.py
@@ -7,16 +7,13 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-def cannyEdgeDetection(
-    image: np.ndarray, sigma: int | float, filter_shape, image_format='rgb',
-    lowthreshold: float | int = 0.05, highthreshold: float | int = 0.09
-):
+def cannyEdgeDetection(image: np.ndarray, sigma: int | float, filter_shape, image_format='rgb', lowthreshold: float | int = 0.05, highthreshold: float | int = 0.09):
     G, theta = sobel_edge_detection(image, sigma, image_format, filter_shape)
     img = non_max_suppression(G, theta)
     img = threshold_hysteresis(img, lowthreshold, highthreshold)
     return img
 
-path = './image3.jpg'
+path = './image1.jpg'
 img = np.array(Image.open(path))
 res = cannyEdgeDetection(img, 1.5, (10, 10), 'rgb')
 plt.subplot(121)
