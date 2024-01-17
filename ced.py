0a5ff4341f7d253ec5b700bd516388039d322ba2
Giuseppe Piccolo <giuseppe.piccolo@protonmail.com>
2024-01-17 12:32:47 +0100

    fixed double threshold for ced

--- ced.py
+++ ced.py
@@ -1,6 +1,7 @@
 from sobel_edge_detection import sobel_edge_detection
 from non_max_suppression import non_max_suppression
 from threshold_hysteresis import threshold_hysteresis
+from double_threshold import double_threshold
 import numpy as np
 from to_gray import togray
 
@@ -9,5 +10,5 @@ def ced(image: np.ndarray, image_format='rgb', lowthreshold: float | int = 0.05,
     grayscale_img = togray(image, image_format)
     G, theta = sobel_edge_detection(grayscale_img, "euclidean")
     img = non_max_suppression(G, theta)
-    img = threshold_hysteresis(img, lowthreshold, highthreshold)
+    img = double_threshold(img, lowthreshold, highthreshold)
     return img
---
+++ double_threshold.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def double_threshold(img: np.ndarray, lowThresholdRatio=0.05, highThresholdRatio=0.09, weak=np.int32(25)):
+    highThreshold = img.max() * highThresholdRatio
+    lowThreshold = highThreshold * lowThresholdRatio
+    
+    M, N = img.shape
+    res = np.zeros((M,N), dtype=np.int32)
+    
+    weak = np.int32(25)
+    strong = np.int32(255)
+    
+    strong_i, strong_j = np.where(img >= highThreshold)
+    
+    weak_i, weak_j = np.where((img <= highThreshold) & (img >= lowThreshold))
+    
+    res[strong_i, strong_j] = strong
+    res[weak_i, weak_j] = weak
+    
+    return res
