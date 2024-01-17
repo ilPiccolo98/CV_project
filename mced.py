be0f6fbe00108a42b7583b0ed4007fcf520d0603
Giuseppe Piccolo <giuseppe.piccolo@protonmail.com>
2024-01-17 18:57:39 +0100

    added zero padding

--- mced.py
+++ mced.py
@@ -9,8 +9,9 @@ from to_gray import togray
 def mced(image: np.ndarray, sigma: int | float, filter_shape, image_format='rgb', lowthreshold: float | int = 0.05, highthreshold: float | int = 0.09):
     img = togray(image, image_format)
     blurred = gaussianBlur(img, sigma, filter_shape=filter_shape)[1] / 255
-    G, theta = sobel_edge_detection(blurred, "absolute")
+    squeezed_blurred = np.squeeze(blurred)
+    padded = np.pad(squeezed_blurred, pad_width=1)
+    G, theta = sobel_edge_detection(padded, "absolute")
     img = non_max_suppression(G, theta)
     img = threshold_hysteresis(img, lowthreshold, highthreshold * 1.562)
-    img = np.pad(img, pad_width=1)
     return img
